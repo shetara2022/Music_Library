adfbbce16128609b65e856517212a482c715d90d
shetara2022 <smithshetarall@gmail.com>
2022-09-24 17:48:39 -0400

    updated album and artist data

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,16 +1,37 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about a given album
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 
-function AlbumView() {
+const AlbumView = (props) => {
     const { id } = useParams()
+    const navigate = useNavigate()
     const [albumData, setAlbumData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const API_URL = `http://localhost:4000/song/${id}`
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => { navigate.push('/') }}>Home</button> | <button onClick={() => navigate.goBack()}>Back</button>
+            </div>
+        )
+    }
+
+    const allAlbums = albumData.filter(entity => entity.kind === 'song')
+        .map((album, i) => { return (<div key={i}>{album.trackName}</div>) })
 
     return (
         <div>
-            <h2>The id passed was {id}</h2>
-            <p>Album Data Goes Here!</p>
+            {albumData.length > 0 ? <h2>{albumData[0].collectionName}</h2> : <p>loading...</p>}
+            {navButtons()}
+            {allAlbums}
         </div>
     )
 }
